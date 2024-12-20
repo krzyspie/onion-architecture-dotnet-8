b620c57f771acbc1bdaa1d57a4c1e41a64ddeaa2
krzpie <krzys_7@op.pl>
2024-12-20 16:56:27 +0100

    Add solution. Add Core project. Add unit tests project.

---
+++ src/UnitTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using ChurchBulletin.Core;
+
+namespace ChurchBulletin.UnitTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
